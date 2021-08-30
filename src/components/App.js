1a9fc723b7038bf61ed3acbff6aa7e34669ab0c0
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-29 19:29:38 -0700

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
-
+import Header from './common/Header';
 
 function App() {
   return (
     <div className="container-fluid">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
-  )
+  );
 }
 
 export default App;
