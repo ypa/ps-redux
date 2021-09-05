aac01ed6faa91f9c9d50dc31a50b16a00d8efe35
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-04 20:15:31 -0700

    Format code prettier

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,38 +10,38 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: './src/index',
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, 'build'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     stats: 'minimal',
     overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
-    headers: {"Access-Control-Allow-Origin": "*"},
-    https: false
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://localhost:3001'),
     }),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      favicon: "src/favicon.ico"
-    })
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
-}
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
