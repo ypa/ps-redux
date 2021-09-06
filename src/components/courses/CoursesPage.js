9ddd83547da20fdc9798c647bb3ad47f31804d4f
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-05 19:29:31 -0700

    Refactor CoursesPage with redux thunk with actionCreators and reducers

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,38 +5,20 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: '',
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-    console.log(this.state.course.title);
-  };
+  componentDidMount() {
+    this.props.actions.loadCourses().catch((error) => {
+      alert('Loading courses failed' + error);
+    });
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
