6a43fdcf97904f0889ce91a6c339059f628c220f
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-11 19:39:52 -0700

    Populate course on course update form

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -23,6 +23,8 @@ function ManageCoursePage({
       loadCourses().catch((error) => {
         alert('Loading courses failed' + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -30,7 +32,7 @@ function ManageCoursePage({
         alert('Loading authors failed' + error);
       });
     }
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -68,9 +70,19 @@ ManageCoursePage.propTypes = {
   history: PropTypes.object.isRequired,
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   };
