3f4036566756614507927988e7edbd5db2a91e58
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-11 19:42:12 -0700

    Make api response time to 2 seconds

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -29,8 +29,8 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 // Simulate delay on all requests
-server.use(function(req, res, next) {
-  setTimeout(next, 0);
+server.use(function (req, res, next) {
+  setTimeout(next, 2000);
 });
 
 // Declaring custom routes below. Add custom routes before JSON Server router
