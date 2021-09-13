c98988c1a0aefcfcbed0bea8808a878205ad23e3
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-12 19:56:21 -0700

    Add loading spinner

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
 import { Redirect } from 'react-router-dom';
+import Spinner from '../common/Spinner';
 
 class CoursesPage extends React.Component {
   state = {
@@ -33,14 +34,21 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.courses} />
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+
+            <CourseList courses={this.props.courses} />
+          </>
+        )}
       </>
     );
   }
@@ -50,6 +58,7 @@ CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -65,6 +74,7 @@ function mapStateToProps(state) {
             };
           }),
     authors: state.authors,
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import * as authorApi from '../../api/authorApi';
+import { beginApiCall } from './apiStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -11,6 +12,7 @@ export function loadCoursesSuccess(courses) {
 
 export function loadAuthors() {
   return function (dispatch) {
+    dispatch(beginApiCall());
     return authorApi
       .getAuthors()
       .then((authors) => {
